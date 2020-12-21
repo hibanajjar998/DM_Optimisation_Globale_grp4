db3e92fadddde168fb41729f5295085fda846fcc
hibanajjar998 <hiba.najjar4@etu.univ-lorraine.fr>
2020-12-21 16:15:37 +0100

    add timed versions off optimisation algos

--- BoxConstrainedGO_Algorithms.py
+++ BoxConstrainedGO_Algorithms.py
@@ -2,6 +2,7 @@ import pyomo.environ as pe
 from pyomo.opt import SolverFactory
 from pyomo.opt import SolverStatus, TerminationCondition
 import os, sys
+import time
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'../utilities')))
 from optmodel_utilities import *
 from tkinter import Tk, Canvas, Button, Toplevel, filedialog
@@ -27,9 +28,9 @@ def savegraph(canvas,window):
 
         
 
-def PlotModel(window, model, level='on',type_opt="", solver_name=""):
+def PlotModel(window, model, level='root',type_opt="", solver_name=""):
     """
-    level == 'on' : create canvas on the window
+    level == 'root' : create canvas on the window
     level == 'ontop' : vreate another level on top of the window
     type_opt in {'PRS', 'MS', 'MBH}  """
     
@@ -47,7 +48,7 @@ def PlotModel(window, model, level='on',type_opt="", solver_name=""):
     ball_color = "#F1C40F"
     
     # fill the tkinter window:
-    if level=='on' : root = window
+    if level=='root' : root = window
     else: root = Toplevel(window)
     title = type_opt+" - Solver "+solver_name+" - n = "+str(n)
     root.title(title)
@@ -91,6 +92,26 @@ def perturb_point(model, gen_pert, epsilon = 0.3):
         model.x[i] = max(0, min(model.x[i].value, 1))
         model.y[i] = max(0, min(model.y[i].value, 1))
 
+# adjusted perturbation
+def perturb_point_adj(model, gen_pert):
+    epsilon = 0.01*model.r.value
+    for i in model.N:
+        model.x[i] = model.x[i].value*(1+gen_pert.uniform(-0.5, 0.5) * epsilon)
+        model.y[i] = model.y[i].value*(1+gen_pert.uniform(-0.5, 0.5) * epsilon)
+        #project inside the box (ATTENTION: the perturbation is not anymore a uniform distribution)
+        model.x[i] = max(0, min(model.x[i].value, 1))
+        model.y[i] = max(0, min(model.y[i].value, 1))
+
+
+
+
+
+
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Pure Random Search
+
 
 # this function performs a pure random search on a model and
 # for a given number of iterations
@@ -126,6 +147,42 @@ def purerandomsearch(mymodel, iter, gen_multi, labels,
 
     return True
 
+# this function performs a timed version ofpure random search 
+def purerandomsearch_timed(mymodel, time_limit, gen_multi, labels,
+                     logfile = None, epsilon=10**-4):
+
+    algo_name = "PRS:"
+    best_obj = 10000 #put a reasonable value bound for the objective
+    bestpoint = {} #dictionary to store the best solution
+    
+    nb_solution = 0
+    time_start = time.time()
+    it = 0
+    
+    while time.time()-time_start < time_limit :
+        it+=1
+        random_point(mymodel, gen_multi)
+
+        #we trust random generation to produce a feasible point
+        #otherwise we need to check feasibility
+        
+        # printing result and solution on screen
+        obj = mymodel.obj()
+        print(algo_name + " Iteration ", it, " current value ", obj, end = '', file = logfile)
+        if obj < best_obj -epsilon:  # + epsilon:
+            best_obj = obj
+            print(" *", file = logfile)
+            printPointFromModel(mymodel, logfile)
+            StorePoint(mymodel, bestpoint, labels)
+        else:
+            print(file = logfile)
+
+    print(algo_name +" Best record found  {0:8.4f}".format(best_obj))
+    LoadPoint(mymodel, bestpoint)
+    printPointFromModel(mymodel)
+
+    return True
+
 def check_if_optimal(results):
     ok = (results.solver.status == pe.SolverStatus.ok)
     optimal = (results.solver.termination_condition
@@ -133,6 +190,14 @@ def check_if_optimal(results):
     return  (ok and optimal)
 
 
+
+
+
+
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# MULTISTART 
 
 # this function performs multistart on a model and
 # for a given number of iterations
@@ -178,6 +243,63 @@ def multistart(mymodel, iter, gen_multi, localsolver, labels,
     print(algo_name + " Total number of feasible solutions ", nb_solution)
 
     return feasible
+
+
+# this function performs a timed version of multistart 
+def multistart_timed(mymodel, time_limit, gen_multi, localsolver, labels,
+               logfile = None, epsilon = 10**-4):
+
+    algo_name = "Multi:"
+    best_obj = 10000 #put a reasonable value bound for the objective
+    bestpoint = {} #dictionary to store the best solution
+    
+    nb_solution = 0
+    feasible = False
+    time_start = time.time()
+    it = 0
+    
+    while time.time()-time_start < time_limit :
+        it+=1
+        random_point(mymodel, gen_multi)
+
+        # local search
+        results = localsolver.solve(mymodel)
+
+        # printing result and solution on screen
+        if check_if_optimal(results):
+            nb_solution += 1  # couting feasible iterations
+            obj = mymodel.obj()
+            print(algo_name + " Iteration ", it, " current value ", obj, end = '', file = logfile)
+            if obj < best_obj - epsilon:  # + epsilon:
+                best_obj = obj
+                print(" *" , file = logfile)
+                printPointFromModel(mymodel, logfile)
+                feasible = True
+                StorePoint(mymodel, bestpoint, labels)
+            else:
+                print(file = logfile)
+        else:
+            print(algo_name+" Iteration ", it, "No feasible solution", file = logfile)
+
+    if feasible == True:
+        print(algo_name + " Best record found  {0:8.4f}".format(best_obj))
+        LoadPoint(mymodel, bestpoint)
+        printPointFromModel(mymodel)
+    else:
+        print(algo_name + " No feasible solution found by local solver")
+        
+    print(algo_name + " Total number of feasible solutions ", nb_solution)
+
+    return feasible
+
+
+
+
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# MBH
+
 
 # this function performs MBH on a model
 def MBH(mymodel, gen, localsolver, labels,
@@ -257,6 +379,91 @@ def MBH_MultiTrial(mymodel, iter, gen, localsolver, labels,
     feasible = False
 
     for it in range(1,iter+1):
+        no_improve = 0
+        nb_solution = 0
+
+        random_point(mymodel, gen)
+
+        # local search
+        results = localsolver.solve(mymodel)
+
+        # a first feasible solution is found
+        if check_if_optimal(results):
+            nb_solution += 1  # couting feasible iterations
+            current_value = mymodel.obj()
+            StorePoint(mymodel, currentcenter, labels)
+            
+            print(algo_name + " ", it, " starting center "," current value ", current_value, end = '', file = logfile)
+
+            #saving GO
+            if current_value < best_obj - epsilon:
+                best_obj = current_value
+                print(" *", file = logfile)
+                #printPointFromModel(mymodel)
+                feasible = True
+                StorePoint(mymodel, bestpoint, labels)
+            else:
+                print(file = logfile)
+            
+            #start local search (perturbation of the current point)
+            while (no_improve < max_no_improve):
+                
+                perturb_point(mymodel, pert)
+                results =localsolver.solve(mymodel)
+                obj = mymodel.obj()
+
+                if check_if_optimal(results):
+                    #improving on current center
+                    if obj < current_value - epsilon:
+                        current_value = obj
+                        print(algo_name + " Trial ", it, " no_improve ", no_improve, " current value ", current_value, end ='', file = logfile)
+                        #no load needed - in model there is already the perturbed point
+                        no_improve = 0
+                        #improving also on global solution
+                        if obj < best_obj - epsilon:
+                            best_obj = obj
+                            print(" *", file = logfile)
+                            feasible = True
+                            StorePoint(mymodel, bestpoint, labels)
+                        else:
+                            print(file = logfile)
+                    else:
+                        #restoring current center
+                        LoadPoint(mymodel, currentcenter)
+                        print(algo_name + " ", it, " no_improve ", no_improve, " noImprovingStep", file = logfile)
+                        no_improve += 1
+        else:
+            print(algo_name + " ", it, " no_improve ", no_improve, "No feasible solution", file = logfile)
+
+    if feasible == True:
+        print(algo_name + " Best record found  {0:8.4f}".format(best_obj))
+        LoadPoint(mymodel, bestpoint)
+        printPointFromModel(mymodel)
+    else:
+        print(algo_name + " No feasible solution found by local solver")
+        
+    print("Multi:Total number of feasible solutions ", nb_solution)
+
+    return feasible
+
+
+# this function performs a timed version of multi trial MBH
+def MBH_MultiTrial_timed(mymodel, time_limit, gen, localsolver, labels,
+        max_no_improve, pert, delta,
+        logfile = None, epsilon = 10**-4):
+
+    algo_name = "MBH:Trial"
+    best_obj = 10000 #put a reasonable value bound for the objective
+    bestpoint = {} #dictionary to store the best solution
+    current_value = best_obj
+    currentcenter = {} #dictionary to store the current center (for pertubation step)
+    
+    feasible = False
+    time_start = time.time()
+    it = 0
+    
+    while time.time()-time_start < time_limit :
+        it+=1
         no_improve = 0
         nb_solution = 0
 
--- TestingAlgorithms.py
+++ TestingAlgorithms.py
@@ -12,7 +12,7 @@ import random
 import time
 
 #Testing PRS vs Multistart
-def main_PRS_MS(n, solver_name='snopt'):
+def main_PRS_MS(n, solver_name='snopt', time_limit=0):
 
     Multi_n_iter = n*100
     PRS_n_iter = Multi_n_iter*n*60
@@ -35,7 +35,9 @@ def main_PRS_MS(n, solver_name='snopt'):
 
     gen_multi.seed(seed1)
     # launching multistart
-    FoundSolution = multistart(mymodel, Multi_n_iter, gen_multi, localsolver, labels, logfile)
+    if time_limit==0: FoundSolution = multistart(mymodel, Multi_n_iter, gen_multi, localsolver, labels, logfile)
+    else: FoundSolution = multistart_timed(mymodel, time_limit, gen_multi, localsolver, labels, logfile)
+
     model_MS = mymodel.clone()
     multistart_time = time.process_time()
 
@@ -43,7 +45,9 @@ def main_PRS_MS(n, solver_name='snopt'):
         
     #restarting from same seed
     gen_multi.seed(seed1)
-    purerandomsearch(mymodel, PRS_n_iter, gen_multi, labels, logfile)
+    if time_limit==0: purerandomsearch(mymodel, PRS_n_iter, gen_multi, labels, logfile)
+    else: purerandomsearch_timed(mymodel, time_limit, gen_multi, labels, logfile)
+
     model_PRS = mymodel.clone()
     prs_time = time.process_time()
 
@@ -57,7 +61,7 @@ def main_PRS_MS(n, solver_name='snopt'):
 
 
 # Testing MBH
-def main_MBH(n,solver_name='snopt'):
+def main_MBH(n,solver_name='snopt', iter=100, mbh_multitrial=False, time_limit=0):
     
     max_no_improve = n*2
 
@@ -84,7 +88,10 @@ def main_MBH(n,solver_name='snopt'):
 
     logfile = open("mylog.txt", 'w')
     # launching multistart
-    FoundSolution = MBH(mymodel, gen, localsolver, labels, max_no_improve, pert, delta, logfile)
+    if mbh_multitrial:
+        if time_limit == 0: FoundSolution = MBH_MultiTrial(mymodel, iter, gen, localsolver, labels, max_no_improve, pert, delta, logfile)
+        else: FoundSolution = MBH_MultiTrial_timed(mymodel, time_limit, gen, localsolver, labels, max_no_improve, pert, delta, logfile)
+    else: FoundSolution = MBH(mymodel, gen, localsolver, labels, max_no_improve, pert, delta, logfile)
 
     mbh_time = time.process_time()
     
@@ -98,19 +105,26 @@ def main_MBH(n,solver_name='snopt'):
 
 
 
+
+
+
 if __name__ == '__main__':
-    n = 3
-    solver_name = 'snopt'
+    n = 2
+    solver_name = "minos" #,"loqo" , "lgo"  ,'knitro' , 'conopt' , 'snopt'
+    iter = 10
+    mbh_multitrial = True
+    # if time_limit==0, the optimisation is not timed
+    time_limit = 2 #(in seconds)
     
     # run different optimisation algorithms
-    model_PRS, model_MS = main_PRS_MS(n,solver_name)
-    MBH_model = main_MBH(n,solver_name)
+    model_PRS, model_MS = main_PRS_MS(n,solver_name, time_limit)
+    MBH_model = main_MBH(n,solver_name, iter, mbh_multitrial, time_limit)
     
     # plot the results
     window=Tk()      
-    PlotModel(window, model_PRS,'on', "Pure Random Search", solver_name)
+    PlotModel(window, model_PRS,'root', "Pure Random Search", solver_name)
     PlotModel(window, model_MS,'ontop', "MultiStart", solver_name)
-    #PlotModel(window, MBH_model,'ontop', "MBH", solver_name)
+    PlotModel(window, MBH_model,'ontop', "MBH", solver_name)
     window.mainloop()
 
 
